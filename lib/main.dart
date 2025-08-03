fd40ef8ba16bcfb4ae530a3baeae4a7e2737f97e
Memo Hurakan <memo.montemayor@gmail.com>
2025-08-02 21:38:15 -0600

    Second Commit (AnimatedOpacity,SingleChildScrollView,FontSize and Color change, No vibration).

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,22 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Animal Counter App',
+      debugShowCheckedModeBanner: false, // Disable debug banner
+      theme: ThemeData.dark(),
       home: AnimalCounter(),
     );
   }
 }
 
 class AnimalCounter extends StatefulWidget {
+  const AnimalCounter({super.key});
+
   @override
   _AnimalCounterState createState() => _AnimalCounterState();
 }
@@ -25,6 +32,20 @@ class _AnimalCounterState extends State<AnimalCounter> {
   int _count = 0;
   StreamSubscription? _accelerometerSubscription;
   DateTime _lastShakeTime = DateTime.now();
+  double opacity = 1.0;
+
+   void animateCountChange(Function updateCount) {
+    setState(() => opacity = 0.0); // se desvanece
+    Future.delayed(Duration(milliseconds: 500), () {
+      updateCount();
+      setState(() => opacity = 1.0); // vuelve a aparecer
+    });
+  }
+
+  void increment() => animateCountChange(() => _count++);
+  void decrement() => animateCountChange(() => _count--);
+  void reset() => animateCountChange(() => _count = 0);
+  
 
   @override
   void initState() {
@@ -47,7 +68,7 @@ class _AnimalCounterState extends State<AnimalCounter> {
         if (difference.inMilliseconds > 1000) {
           // At least 1 second passed since last shake
           setState(() {
-            _count++;
+            _count+=10; // Increment count by 10
           });
           _lastShakeTime = now;
         }
@@ -64,12 +85,39 @@ class _AnimalCounterState extends State<AnimalCounter> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Contador', style: TextStyle(fontSize: 10)
+      ),
+      centerTitle: true, // Center the title
+      ),
       backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          '$_count',
-          style: TextStyle(fontSize: 64, color: Colors.green),
+      body: SafeArea(
+      child: SingleChildScrollView(
+       child:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Animales Contados:', style: TextStyle(fontSize: 8)),
+            AnimatedOpacity(
+              duration: Duration(milliseconds: 200),
+              opacity: opacity,
+               child: Text(
+                '$_count',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: const Color.fromARGB(255, 243, 2, 2)),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(icon: Icon(Icons.remove), onPressed: decrement),
+                IconButton(icon: Icon(Icons.refresh), onPressed: reset),
+                IconButton(icon: Icon(Icons.add), onPressed: increment),
+              ]
+              ),
+          ]
         ),
+      ),
+      )
       ),
     );
   }
